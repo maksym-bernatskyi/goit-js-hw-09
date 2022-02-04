efa34de8e8293298b60849cbc98ed9606bc44415
Maksym Bernatskyi <bernasik007@gmail.com>
2022-02-04 20:24:28 +0200

    HW-09 task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+    buttonStart: document.querySelector('button[data-start]'),
+    buttonStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+};
 
+disableButtonStop();
+let timerId = null;
+
+refs.buttonStart.addEventListener('click', onButtonStartClick);
+refs.buttonStop.addEventListener('click', onButtonStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function disableButtonStart() {
+    refs.buttonStart.setAttribute('disabled', 'disabled');
+}
+
+function disableButtonStop() {
+    refs.buttonStop.setAttribute('disabled', 'disabled');
+}
+
+function onButtonStartClick() {
+    disableButtonStart();
+    refs.buttonStop.removeAttribute('disabled');
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onButtonStopClick() {
+    disableButtonStop();
+    refs.buttonStart.removeAttribute('disabled');
+    clearInterval(timerId);
+}
